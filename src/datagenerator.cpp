aecc25528805a33f4d15274832ba3e1dfafefe48
osbjorkg <osbjorkg@abo.fi>
2022-09-27 15:50:00 +0300

    Fixes to input data generator

--- src/datagenerator.cpp
+++ src/datagenerator.cpp
@@ -20,6 +20,7 @@ void generateVesselData(unsigned int nrOfSamples, int sampleWidth, int sampleHei
     unsigned char *header;
     header = new unsigned char[54];
 
+    // Hardcoded file header for 24-bit 200x200 bmp image.
     header[ 0] = (unsigned char)('B');
     header[ 1] = (unsigned char)('M');
 
@@ -32,20 +33,34 @@ void generateVesselData(unsigned int nrOfSamples, int sampleWidth, int sampleHei
     header[ 7] = (unsigned char)(0);
     header[ 8] = (unsigned char)(0);
     header[ 9] = (unsigned char)(0);
-    
+
     header[10] = (unsigned char)(54);
-//
-    header[40] = (unsigned char)(       40    );
-    header[44] = (unsigned char)(       sampleWidth    );
-    header[45] = (unsigned char)(       sampleWidth>> 8);
-    header[46] = (unsigned char)(       sampleWidth>>16);
-    header[47] = (unsigned char)(       sampleWidth>>24);
-    header[48] = (unsigned char)(       sampleHeight    );
-    header[49] = (unsigned char)(       sampleHeight>> 8);
-    header[50] = (unsigned char)(       sampleHeight>>16);
-    header[51] = (unsigned char)(       sampleHeight>>24);
-    header[52] = (unsigned char)(       1);
-    header[54] = (unsigned char)(       24);
+    header[14] = (unsigned char)(40);
+
+    header[18] = (unsigned char)(sampleWidth); // 0xc8
+    header[22] = (unsigned char)(sampleHeight);// 0xc8
+    header[26] = (unsigned char)(1);
+    header[28] = (unsigned char)(0x18); // Bits per pixel - 24
+
+    // header[34] = (unsigned char)(0xc0);
+    // header[35] = (unsigned char)(0xd4);
+    // header[36] = (unsigned char)(0x01);
+    // header[38] = (unsigned char)(0xc3);
+    // header[39] = (unsigned char)(0x0e);
+    // header[42] = (unsigned char)(0xc3); // biYPelsPerMeter - 0xc3
+    // header[43] = (unsigned char)(0x0e);
+
+    // header[40] = (unsigned char)(       40    );
+    // header[44] = (unsigned char)(       sampleWidth    );
+    // header[45] = (unsigned char)(       sampleWidth>> 8);
+    // header[46] = (unsigned char)(       sampleWidth>>16);
+    // header[47] = (unsigned char)(       sampleWidth>>24);
+    // header[48] = (unsigned char)(       sampleHeight    );
+    // header[49] = (unsigned char)(       sampleHeight>> 8);
+    // header[50] = (unsigned char)(       sampleHeight>>16);
+    // header[51] = (unsigned char)(       sampleHeight>>24);
+    // header[52] = (unsigned char)(       1);
+    // header[54] = (unsigned char)(       24);
 
 
     for(int sampleNr = 0; sampleNr < nrOfSamples; sampleNr++) {
